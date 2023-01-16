9d1550471d0f898876a970b363a31f7a3d35ebed
Email <76654292+duydang2002@users.noreply.github.com>
2023-01-16 16:06:39 +0700

    Fix Bug1

--- src/Controller/MainStage.java
+++ src/Controller/MainStage.java
@@ -9,7 +9,7 @@ import javafx.stage.StageStyle;
 
 public class MainStage extends Application {
     public static void main(String[] args) {
-        Application.launch(args);
+        launch(args);
     }
 
     @Override
--- src/Controller/SceneController.java
+++ src/Controller/SceneController.java
@@ -1,5 +1,6 @@
 package Controller;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -8,15 +9,18 @@ import javafx.stage.StageStyle;
 import java.awt.event.ActionEvent;
 import java.io.IOException;
 
+
 public class SceneController {
     private Stage stage;
     private Parent root;
     private Scene scene;
 
-    public void ThemHoaDon(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("../View/Main.fxml"));
-        stage.initStyle(StageStyle.DECORATED);
-        scene = new Scene(root,400,400);
+
+
+    public void ThemHoaDon(javafx.event.ActionEvent actionEvent) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("../View/ThemDonHangScene.fxml"));
+        stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
